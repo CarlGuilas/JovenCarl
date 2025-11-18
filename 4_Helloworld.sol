779402807b3dfe148a2a2fbcbad5a89f1e5a987a
CarlGuilas <jovencarlcosme@gmail.com>
2025-11-18 14:17:14 +0800

    Add files via upload

---
+++ 4_Helloworld.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal
+pragma solidity ^0.8.20;
+
+contract Helloworld {
+    string public greet = "HelloWorld!";
+}
---
+++ 5_Valuetypes.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Data types - values and references
+
+contract Valuetypes {
+    bool public b = true;
+    uint public u = 123; // uint = uint256 0 to 2**256 - 1
+                         //        uint8   0 to 2**0 - 1
+                         //        uint16  0 to 2**16 - 1
+    int public i = -123; // int = int256  -2**255 to 2**255 - 1
+                         //       int128  -2**127 to 2**127 - 1
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+    address public addr = 0xf6ac838863A3031Bc9c0a1AB751199995f0F2d73;
+    bytes32 public b32 = 0x89c58ced8a9078bdef2bb60f22e58eeff7dbfed6c2dff3e7c508b629295926fa;
+}
